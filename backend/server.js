aca8f5952f91746c33d1b3eaf95fba62687bd739
bernaz romain <bernazromain@gmail.com>
2024-09-20 10:21:54 +0200

    Ajout des informations pour la mise en production

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,14 @@ const MONGODB_URI = isProduction ? process.env.MONGODB_URI_PROD : process.env.MO
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
+
 // Configuration CORS
 const corsOptions = {
-  origin: isProduction 
-    ? process.env.FRONTEND_URL // URL de votre frontend sur Vercel
-    : 'http://localhost:3000', // URL de développement local
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
   optionsSuccessStatus: 200
 };
 
